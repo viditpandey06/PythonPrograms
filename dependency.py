fec7bec09f6260687a5436bac066a0fd173d3798
[Vidit Pandey] <viditpandey06@gmail.com>
2023-12-01 14:11:33 +0530

    Program on dependency

---
+++ dependency.py
@@ -0,0 +1,19 @@
+class Customer:
+    def __init__(self,name, age, phone_no):
+        self.name=name
+        self.age=age
+        self.phone_no=phone_no
+    def purchase (self, payment):
+        if payment.type =='card':
+            print('Paying by card')
+        elif payment.type == 'e-wallet':
+            print('Paying by wallet')
+        else:
+            print('Paying by cash')    
+
+class Payment:
+    def __init__(self,type):
+        self.type=type
+payement1 = Payment('card')
+c=Customer('Jack',12,1234)
+c.purchase(payement1)
