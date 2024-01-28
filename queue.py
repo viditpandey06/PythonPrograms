3625562617cbd8837e2ba92c26578da131c742d0
[Vidit Pandey] <viditpandey06@gmail.com>
2024-01-28 23:02:58 +0530

    Create queue.py

---
+++ queue.py
@@ -0,0 +1,17 @@
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        if self.is_empty():
+            return None
+        return self.items.pop(0)
+
+    def size(self):
+        return len(self.items)
