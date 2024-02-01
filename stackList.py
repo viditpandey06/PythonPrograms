f58e00f820265a6af1cd96bfefda96e8b73bd5c4
[Vidit Pandey] <viditpandey06@gmail.com>
2024-02-01 23:08:33 +0530

    Create stackList.py

---
+++ stackList.py
@@ -0,0 +1,27 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+        else:
+            return None
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def size(self):
+        return len(self.items)
+
+# Example usage:
+stack = Stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+print("Pop:", stack.pop())
+print("Pop:", stack.pop())
+print("Size:", stack.size())
